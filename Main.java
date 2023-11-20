31ddef4acd548e9d0d80694548afbfa775ba46fe
anes rachedi <Anes Rachedi>
2023-11-20 23:36:15 +0100

    Ajout d'un début De GUI pour tester et modification du Main temporaire

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import UI.OctoPunks;
+
 public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -12,29 +14,32 @@ public class Main {
 
 
     public static void main(String[] args) {
-        LexicalAnalyser l = new LexicalAnalyser();
-        while(true){        
-            String out = getOutput();
-            String[] StringCmd = out.split(" ");
-            Command cmd = l.argsToCommand(StringCmd);
+        if(args.length > 0 && "1".equals(args[0])){ //FAITES java Main 1 si vous voulez utiliser les commandes terminales sinon  java Main
+            LexicalAnalyser l = new LexicalAnalyser();
+            while(true){        
+                String out = getOutput();
+                String[] StringCmd = out.split(" ");
+                Command cmd = l.argsToCommand(StringCmd);
 
-            if (StringCmd[0].toUpperCase().equals("EXIT")) return;
-            
-            if(!l.isCommand(cmd)){
-                System.out.println("Commande " + "<" + StringCmd[0] + ">" + " Inconnue");
-                continue;
-            }
+                if (StringCmd[0].toUpperCase().equals("EXIT")) break;
+                
+                if(!l.isCommand(cmd)){
+                    System.out.println("Commande " + "<" + StringCmd[0] + ">" + " Inconnue");
+                    continue;
+                }
 
-            System.out.println("Commande Valide");
-            if(!l.isValidCommand(cmd)){
-                System.out.println("ERREUR ARGUMENTS : Commande " + "<" + StringCmd[0] + ">" + " De Type " + cmd.getType() + " Doit recevoir " + cmd.expectedArgs() + " Arguments ");
-                System.out.println("Utilisation : " + StringCmd[0] + " a(R/N)" + " b(R/N)" + " dest(R)" );
-                System.out.println("INFORMATION: \nAjoute a + b et sauvegarde le résultat dans dest. a, b et dest peuvent être le même registre local. Le résultat sera limité entre -9999 et 9999.");
-                continue;
+                System.out.println("Commande Valide");
+                if(!l.isValidCommand(cmd)){
+                    System.out.println("ERREUR ARGUMENTS : Commande " + "<" + StringCmd[0] + ">" + " De Type " + cmd.getType() + " Doit recevoir " + cmd.expectedArgs() + " Arguments ");
+                    System.out.println("Utilisation : " + StringCmd[0] + " a(R/N)" + " b(R/N)" + " dest(R)" );
+                    System.out.println("INFORMATION: \nAjoute a + b et sauvegarde le résultat dans dest. a, b et dest peuvent être le même registre local. Le résultat sera limité entre -9999 et 9999.");
+                    continue;
+                }
+                // Ajout des Tests de registre et des variables
+                System.out.println("Arguments Correcte");
             }
-            // Ajout des Tests de registre et des variables
-            System.out.println("Arguments Correcte");
-        }   
+        }
+        OctoPunks window = new OctoPunks();
 
     }
 }
---
+++ UI/OctoPunks.java
@@ -0,0 +1,109 @@
+package UI;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class OctoPunks extends JFrame {
+
+    private int width = 1980;
+    private int height = 1080;
+    private JTextArea textArea;
+    private JButton execute;
+    private JButton pause;
+    private JButton step;
+
+    public OctoPunks() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("OctoPunks");
+        this.setBounds(0, 0, width, height);
+        this.setLayout(new BorderLayout());
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        JPanel spacer = new JPanel();
+        spacer.setOpaque(false);
+        spacer.setPreferredSize(new Dimension(width, 20));
+
+
+        textArea = new JTextArea(20, 20);
+        textArea.setBackground(Color.decode("#19171c"));
+        textArea.setForeground(Color.decode("#7d7f88"));
+        textArea.setFont(new Font("Consolas", Font.PLAIN, 20));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+
+        execute = createButton("resources/play.png", Color.decode("#868f9e"), Color.DARK_GRAY);
+        pause = createButton("resources/pause.png", Color.decode("#868f9e"), Color.DARK_GRAY);
+        step = createButton("resources/step.png", Color.decode("#868f9e"), Color.DARK_GRAY);
+
+        execute.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String[] test = textArea.getText().split(" ");
+                System.out.println(test[0]);
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(0, 0, 350, 0); 
+        buttonPanel.add(pause, gbc); 
+        gbc.insets = new Insets(0, 0, 350, 0);
+        buttonPanel.add(execute, gbc);
+        gbc.insets = new Insets(0, 0, 350, 0);
+        buttonPanel.add(step, gbc);
+
+        this.add(textArea, BorderLayout.WEST); 
+        this.add(buttonPanel, BorderLayout.SOUTH); 
+        this.add(spacer, BorderLayout.NORTH);
+        this.add(textArea, BorderLayout.WEST);
+
+        this.setVisible(true);  
+    }
+
+
+    private JButton createButton(String path, Color normalColor, Color hoverColor) {
+        ImageIcon originalIcon = null;
+        try {
+            originalIcon = new ImageIcon(path);
+            if (originalIcon.getImageLoadStatus() != MediaTracker.COMPLETE) {
+                throw new IOException("Image load failed.");
+            }
+        } catch (Exception e) {
+            System.err.println("Error loading image: " + e.getMessage());
+            originalIcon = new ImageIcon(); // Create an empty icon as fallback
+        }
+    
+        // Resize the icon
+        Image image = originalIcon.getImage();
+        Image resizedImage = image.getScaledInstance(35, 35, Image.SCALE_REPLICATE);
+        ImageIcon icon = new ImageIcon(resizedImage);
+    
+        JButton button = new JButton(icon);
+        button.setFont(new Font("Arial", Font.BOLD, 15));
+        button.setBackground(normalColor);
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setPreferredSize(new Dimension(40, 40)); // Set preferred size, slightly larger than the image
+    
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setBackground(hoverColor);
+                button.setForeground(normalColor);
+            }
+    
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setBackground(normalColor);
+                button.setForeground(hoverColor);
+            }
+        });
+    
+        return button;
+    }
+    
+}
