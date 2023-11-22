19d7697a4f069c1c19bc1e0a3c9b7aab63f01aba
Rachedi Anes <12201967>
2023-11-22 17:31:49 +0100

    AJout de quelques commentaires pour la comprehension du fichier COmpilator.java

--- Compilation/Compilator.java
+++ Compilation/Compilator.java
@@ -6,6 +6,7 @@ public class Compilator {
     private int lineNumber;
     private int currentLine;
 
+    //compiles toutes les lignes doit etre reset a la fin
     public Compilator(String s, int mode) {
         String[] text = s.split("\n");
         lines = new Command[text.length];
@@ -13,6 +14,27 @@ public class Compilator {
         for (int i = 0; i < text.length; i++) {
             lines[i] = l.argsToCommand(text[i].split(" "));
         }
+        /*
+        * appel a une nouvelle classe arugmentChecker qui doit verifier 
+        * si les arguments sont correctes
+        */
+        
+        /*
+         * Appel a la methode sendSignal(Command cmd) qui appel l'instruction
+         * cmd.getInstruction() a sa propre methode
+        */
+
+        /*
+         * Appel a une nouvelle methode qui reset les variables de Compilator
+         */
     }
+
+    /*
+    * Constructio, compile ligne par ligne doit attendre un 
+    * second appel de compileNextLine()
+    * pour compiler les lignes d'apres 
+    */
+
+
 
 }
