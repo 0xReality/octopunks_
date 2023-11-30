c71601c8383e0c3e6f5659e87ed584db26681660
Ayoub HAKIM <12107667@f203-16.ig-edu.univ-paris13.fr>
2023-11-30 15:20:15 +0100

    solution

---
+++ Missions/Solution.java
@@ -0,0 +1,60 @@
+package Missions;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Solution {
+    private int mission;
+    private File file;
+    private String fileName;
+    private String s[];
+
+    public Solution(int mission){
+        this.mission = mission;
+        this.fileName = "Solutions/solution" + mission + ".txt";
+        file = new File(fileName);
+        s = loadFile(file).split("\n");
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public File getFile() {
+        return file;
+    }
+  
+    public Boolean CompareTo(String[] s2){
+        if (Arrays.equals(s, s2)){
+            return true;
+        }
+        return false;
+    }
+
+
+
+     public static String loadFile(File f) {
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(f));
+            StringWriter out = new StringWriter();
+            int b;
+            while ((b=in.read()) != -1)
+                out.write(b);
+            out.flush();
+            out.close();
+            in.close();
+            return out.toString();
+        }
+        catch (IOException ie)
+        {
+            ie.printStackTrace(); 
+        }
+        return null;
+    }
+}
+
