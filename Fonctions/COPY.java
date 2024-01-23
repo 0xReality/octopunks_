fe1db9e1a660871166d8c05587fc0fc6b35b92dc
Zakraoui Zakaria <12112191>
2024-01-23 13:06:18 +0100

    modification des classes Fonctions

--- Fonctions/COPY.java
+++ Fonctions/COPY.java
@@ -9,10 +9,6 @@ public class COPY{
 
     public COPY(Register R1, Register R2)
     {
-        if(R1.equals(R2))
-        {
-            throw new IllegalArgumentException("Les deux registres sont les mêmes !")
-        }
         this.registre1 = R1; 
         this.registre2 = R2; 
         registre2.setValeur(registre1.getValeur());
